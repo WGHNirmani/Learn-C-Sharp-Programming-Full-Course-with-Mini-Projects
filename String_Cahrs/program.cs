c6a7fa6943ecc8147c9dac51294a51bfa56f18df
Hirunika Nirmani <hirunikanirmani26@gmail.com>
2024-09-22 02:59:17 +0530

    Create program.cs

---
+++ String_Cahrs/program.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace String_Chars
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string name = "Aba";
+            char letter = 'a';
+
+            Console.WriteLine("Your name is: ");
+            Console.WriteLine(name);
+
+            Console.WriteLine();
+            Console.WriteLine(letter);
+
+            Console.ReadLine();
+        }
+    }
+}
