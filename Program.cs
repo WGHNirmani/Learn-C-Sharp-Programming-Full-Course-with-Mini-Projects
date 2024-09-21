18de28480c3012758b82c100b57cc78aa73c375f
Hirunika Nirmani <hirunikanirmani26@gmail.com>
2024-09-21 19:13:52 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Waits for the user to press Enter before closing the program.
+            // This prevents the console window from closing immediately after execution.
+            Console.ReadLine();
+        }
+    }
+}
