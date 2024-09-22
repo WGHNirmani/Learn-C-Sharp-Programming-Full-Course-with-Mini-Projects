f3cb3dbc47afd83a98467ca94825a48ea8d0932e
Hirunika Nirmani <hirunikanirmani26@gmail.com>
2024-09-22 07:24:10 +0530

    Create program.cs

---
+++ BooleanDataType/program.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Bool
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            bool value = true;
+
+            bool isMale = true;
+            Console.WriteLine(isMale);
+
+            isMale = false;
+            Console.WriteLine(isMale);
+
+
+            Console.ReadLine();
+        }
+    }
+}
