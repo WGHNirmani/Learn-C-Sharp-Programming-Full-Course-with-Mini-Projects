a50859a4f42f0b4441d12c36c4e684e95cf0eec0
Hirunika Nirmani <hirunikanirmani26@gmail.com>
2024-09-22 18:49:27 +0530

    Create program.cs

---
+++ Exercise_Storing_User_Data/program.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace Exercise
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            /*
+             * Define a variable to hold your name
+             * Define a variable to hold your phone number
+             * Define a variable to hold your age
+             * Print variables line-by-line to the screen
+             * Extra:define variables using the var keyword
+             */
+
+            //string name = "Hirunika";
+            var name = "Hirunika";
+            //string number = "0761234567";
+            var number = "0761234567";
+            //int age = 24;
+            var age = 24;
+
+            Console.WriteLine(name);
+            Console.WriteLine(number);
+            Console.WriteLine(age);
+
+            Console.ReadLine();
+        }
+    }
+}
