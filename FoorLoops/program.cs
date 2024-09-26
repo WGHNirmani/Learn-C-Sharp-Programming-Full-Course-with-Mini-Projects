6e024a8a4f65b50cb786bbc771e8c330dbfe7273
Hirunika Nirmani <hirunikanirmani26@gmail.com>
2024-09-27 04:56:56 +0530

    Update program.cs

--- FoorLoops/program.cs
+++ FoorLoops/program.cs
@@ -1,1 +1,48 @@
+using System;
+
+namespace For
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            /*Console.WriteLine("Hi");
+            Console.WriteLine("Hi");
+            Console.WriteLine("Hi");
+            Console.WriteLine("Hi");
+            Console.WriteLine("Hi");*/
+
+            Console.Write("What do you want to repeat?: ");
+            string message = Console.ReadLine();
+
+            Console.Write("How many times do you want to repeat it?: ");
+            int loopCounter = Convert.ToInt32(Console.ReadLine());
+
+            if (loopCounter <= 0) // >= 1
+            {
+                Console.WriteLine("Sorry, plase enter a value above 0");
+            }
+            else
+            {
+                for (int i = 0; i < loopCounter; i++)
+                {
+                    //Console.WriteLine("Hi");
+                    Console.WriteLine(message);
+                }
+            }
+
+            /*for (int i = 0; i< 5; i++)
+            {
+                Console.WriteLine("Hi");
+            }*/
+
+            /*for(int i = 0; i<= 10; i += 2)
+            {
+                Console.WriteLine(i);
+            }*/
+
+            Console.ReadLine();
+        }
+    }
+}
 
